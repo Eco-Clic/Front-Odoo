9fccc38cbce6da7532c82c1f4264b8944c566955
samuelfrometa <sel.null.c30@gmail.com>
2025-07-11 11:01:01 +0200

    Cambio para la configuracion del tailwind

--- odoo_frontend/tailwind.config.js
+++ odoo_frontend/tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./App.{js,ts,tsx}', './components/**/*.{js,ts,tsx}'],
-
-  presets: [require('nativewind/preset')],
+  content: ["./App.tsx", 
+    "./src/pages/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
